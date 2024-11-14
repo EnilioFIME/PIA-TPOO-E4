e359346f3291538ca4302acdd19141f9745485f9
Emilio Romero <emilio.romeroc@uanl.edu.mx>
2024-11-13 22:52:17 -0600

    se agregaron muchas cosas, conexion a bd exitosa

--- GymlatorNoRcuerdoElNombre/src/Inicio.java
+++ GymlatorNoRcuerdoElNombre/src/Inicio.java
@@ -1,29 +1,51 @@
-import java.awt.*;
 import javax.swing.*;
-import javax.swing.border.*;
+
+import java.awt.*;
+import java.sql.Connection;
 
 public class Inicio extends JFrame {
 
     public Inicio() {
+
+        //Inicia la ventana del programa y que cierre el programa en cuanto se sale de este
         setTitle("Aplicación de Gestión");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
 
+        //Crea las pestañas (tabs)
         JTabbedPane pestañas = new JTabbedPane();
 
+        //Aqui se declara la fuente a utilizar, el texto de las pestañas
+        Font fuente = new Font("Arial", Font.BOLD, 22);
+        pestañas.setFont(fuente);
+
+        //Las pestañas como tal y a los archivos a los que se dirije
         pestañas.addTab("Asistencias", new PanelAsistencias());
         pestañas.addTab("Usuarios", new PanelUsuarios());
         pestañas.addTab("Entrenadores", new PanelEntrenadores());
         pestañas.addTab("Clases", new PanelClases());
         pestañas.addTab("Máquinas", new PanelMaquinas());
         pestañas.addTab("Pagos", new PanelPagos());
-
         add(pestañas);
-
         setVisible(true);
+
+        //Se inicia una prueba de conexion a la BD
+        pruebaConexion();
     }
 
+    //Como tal aqui es donde ocurre la prueba, solo que se manda a llamar arriba
+    private void pruebaConexion() {
+        Connection connection = ConectorBD.getConnection();
+        if (connection != null) {
+            System.out.println("Conexión exitosa a la base de datos.");
+        } else {
+            System.out.println("Error al conectar a la base de datos.");
+        }
+    }
+
+    //Aqui inicia el programa
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new Inicio());
     }
--- GymlatorNoRcuerdoElNombre/src/PanelUsuarios.java
+++ GymlatorNoRcuerdoElNombre/src/PanelUsuarios.java
@@ -5,11 +5,6 @@ import javax.swing.border.*;
 public class PanelUsuarios extends JPanel {
 
     public PanelUsuarios() {
-        // Configuración del panel
-        setLayout(new BorderLayout());
-        JLabel etiqueta = new JLabel("Sección de Usuarios", JLabel.CENTER);
-        etiqueta.setFont(new Font("Arial", Font.BOLD, 20));
-        add(etiqueta, BorderLayout.CENTER);
-        setBorder(new EmptyBorder(10, 10, 10, 10));
+       
     }
 }
